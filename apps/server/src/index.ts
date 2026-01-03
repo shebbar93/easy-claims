8218918a564fe8e55658e3841105ddb045117c61
shebbar93 <s.hebbar93@gmail.com>
2026-01-03 10:59:41 -0500

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,34 @@
+import { createContext } from "@easy-claims/api/context";
+import { appRouter } from "@easy-claims/api/routers/index";
+import { env } from "@easy-claims/env/server";
+import { trpcServer } from "@hono/trpc-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+  }),
+);
+
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: (_opts, context) => {
+      return createContext({ context });
+    },
+  }),
+);
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+export default app;
---
+++ packages/db/src/index.ts
@@ -0,0 +1,11 @@
+import { env } from "@easy-claims/env/server";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+
+import * as schema from "./schema";
+
+const client = createClient({
+  url: env.DATABASE_URL,
+});
+
+export const db = drizzle({ client, schema });
